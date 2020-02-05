07c2d6c0996cae89dfeb18e209c66709448b48b0
tumerictj <troy.jasso@gmail.com>
2020-02-05 02:09:53 -0800

    Add files via upload
    Updated files to address the feedback below:
        Please make sure that the code is PEP8 compliant.
        The code should run on Python 3.x.
        The code includes hard-coded values for first/last lines. These should be handled algorithmically.

--- program-1.py
+++ program-1.py
@@ -5,47 +5,55 @@
 # compute the temperature spread for each day,
 # and then output the day with the minimal temperature spread
 
-# Imports the regex module
+# The import statement below imports the debugger.
+# It is used with pdb.set_trace() to set breakpoints
+import pdb
+
+# This import statement imports the regex module.
 import re
 
-# Opens the weather file
-print "\nReading weather file...\n"
-weatherfile = open("w_data.dat")
+# The open statement below opens the weather data file.
+print("\nReading weather file...\n")
+weather_file = open("w_data.dat")
 
-# Notes the first lines to read and not read, excluding the header and footer
-# For the weather file, the first line to read is line 7,
-# and the first line to not read is 37
-bodytext = range(7, 37)
+# The commands below store the day of the month and temperature spread
+# for each day.
+weather_data = []
+for each_line in weather_file:
+    # The re command below removes the special characters from 
+    # each_line with regex substitution.
+    each_line = re.sub('[^A-Za-z0-9 ]+', '', each_line)
+    # The split command below splits each_line into columns.
+    values = each_line.split()
+    # The conditional below tests by length and format to ignore the header 
+    # and footer lines.
+    if (len(values) >= 3 
+            and values[1].isnumeric() 
+            and values[2].isnumeric()):
+        # This stores the value in column 1 and the difference between 
+        # columns 2 and 3.
+        weather_data.append([values[0], 
+            float(values[1]) - float(values[2])])
+        # This prints the values in columns 1, 2, and 3, and the 
+        # difference between columns 2 and 3.
+        print("\tDay: %2s" % values[0],
+            ", High: %5s" % float(values[1]),
+            ", Low: %5s" % float(values[2]),
+            ", Spread: %5s" % (float(values[1]) - float(values[2])))
 
-# Stores the day of the month and temperature spread for each day
-linecount = 1
-weatherdata = []
-for eachline in weatherfile:
-    # Ignores the header and footer lines
-    if linecount in bodytext:
-        # Removes the special characters from eachline with regex substitution here
-        eachline = re.sub('[^A-Za-z0-9 ]+', '', eachline)
-        # Splits eachline into columns
-        values = eachline.split()
-        # Stores the value in column 1 and the difference between columns 2 and 3
-        weatherdata.append( [ values[0], float(values[1]) - float(values[2]) ] )
-        # Prints the values in columns 1, 2, and 3, and the difference between columns 2 and 3
-        print "\tDay: %2s" % values[0], ", High: %5s" % float(values[1]), ", Low: %5s" % float(values[2]), ", Spread: %5s" % ( float(values[1]) - float(values[2]) )
+# The commands below find the minimum temperature spread.
+minimal_temperature_spread = weather_data[0][1]
+for each_day in range(len(weather_data)):
+    if weather_data[each_day][1] < minimal_temperature_spread:
+        minimal_temperature_spread = weather_data[each_day][1]
 
-    # Increments the linecount for each line
-    linecount = linecount + 1
+# The below commands find the day or days with the minimum temperature spread.
+minimal_spread_day = []
+for each_day in range(len(weather_data)):
+    if weather_data[each_day][1] == minimal_temperature_spread:
+        minimal_spread_day.append(weather_data[each_day][0])
 
-# Finds the minimum temperature spread
-minimaltemperaturespread = weatherdata[0][1]
-for eachday in range ( len(weatherdata) ):
-    if weatherdata[eachday][1] < minimaltemperaturespread:
-        minimaltemperaturespread = weatherdata[eachday][1]
-
-# Finds the day or days with the minimum temperature spread
-minimalspreadday = []
-for eachday in range ( len(weatherdata) ):
-    if weatherdata[eachday][1] == minimaltemperaturespread:
-        minimalspreadday.append( weatherdata[eachday][0] )
-
-# Prints the day or days with the minimum temperature spread and the minimum temperature spread
-print "\nDay(s) with Minimum Spread: ", minimalspreadday, "; Minimum Spread: ", minimaltemperaturespread, "\n"
+# The below command prints the day or days with the minimum temperature
+# spread and the minimum temperature spread
+print ("\nDay(s) with Minimum Spread: ", minimal_spread_day, 
+        "; Minimum Spread: ", minimal_temperature_spread, "\n")
--- program-2.py
+++ program-2.py
@@ -15,70 +15,102 @@
 # depending on the parameters of the output function.
 
 
-# Imports the debugger
+# The import command below imports the debugger
+# which can be used with the pdb.set_trace command
+# to set a breakpoint in the code
 import pdb
 
-# Imports the regex module
+# The import command below imports the regex module
+# which will be used to remove special characters 
+# from our data
 import re
 
-def MinimumSpreadFinder(filenametoread, firstlinetoread, firstlinenottoread, indexcolumn, minuendcolumn, subtrahendcolumn):
+def minimum_spread_finder(filename_to_read,  
+        index_column, minuend_column, 
+        subtrahend_column):
+    """ This function find the minimum spread 
+        between two columns of a data file"""
 
-    # Opens the soccer file
-    print "\nReading", filenametoread, "file...\n"
-    filetoread = open(filenametoread)
+    # The open command below opens the data file.
+    print("\nReading", filename_to_read, "file...\n")
+    file_to_read = open(filename_to_read)
     
-    # Notes the first lines to read and not read, excluding the header and footer
-    bodytext = range(firstlinetoread, firstlinenottoread)
-    
-    # Stores the index of the month and  spread for each index
-    linecount = 1
-    filetoreaddata = []
-    for eachline in filetoread:
-        # Ignores the header and footer lines
-        if linecount in bodytext:
-            # Removes the special characters from eachline with regex substitution here
-            eachline = re.sub('[^A-Za-z0-9 ]+', '', eachline)
-            # Splits eachline into columns
-            values = eachline.split()
-            # For each line with meaningful data
-            if len(values) > 0:
-                # Stores the value in the index column and the difference between the minuend column and the subtrahend column
-                # The reason for subtracting one from the values of the columns is that the values array is indexed from 0
-                # so, e.g., column 1, will have position 0, column 2 will have position 1, etc.
-                # The absolute value is used since to ensure the spread is positive
-                filetoreaddata.append( [ values[indexcolumn - 1], abs(float(values[minuendcolumn - 1]) - float(values[subtrahendcolumn - 1])) ] )
+    # The array declared below will store the each index and the spread
+    # for each index
+    file_to_read_data = []
+    for each_line in file_to_read:
+        # The re command below removes the special characters from 
+        # each_line with regex substitution
+        each_line = re.sub('[^A-Za-z0-9 ]+', '', each_line)
+        # The split command below splits each_line into columns
+        values = each_line.split()
+        # The conditional below ignores header and footer lines
+        # by checking for number of columns and format
+        if (len(values) >= 3 
+                and values[minuend_column - 1].isnumeric() 
+                and values[subtrahend_column - 1].isnumeric()):
+                # For each line with meaningful data,
+                # these commands store the value in the index column 
+                # and the difference between the minuend column 
+                # and the subtrahend column.
+                # The reason for subtracting 1 
+                # from the values of the columns 
+                # is that the values array is indexed from 0
+                # so, e.g., column 1, will have position 0, 
+                # column 2 will have position 1, etc.
+                # The absolute value is used 
+                # to ensure the spread is positive
+                file_to_read_data.append([values[index_column - 1],
+                    abs(float(values[minuend_column - 1]) 
+                        - float(values[subtrahend_column - 1]))])
                 # Prints the values in those columns and the difference
-                print "\tIndex: %15s" % values[indexcolumn - 1], ", Minuend: %5s" % float(values[minuendcolumn - 1]), ", Subtrahend: %5s" % float(values[subtrahendcolumn - 1]), ", Spread: %5s" % ( abs( float(values[minuendcolumn - 1]) - float(values[subtrahendcolumn - 1]) ) )
+                print("\tIndex: %15s" % values[index_column - 1],
+                        ", Minuend: %5s" % float(values[minuend_column - 1]),
+                        ", Subtrahend: %5s" % float(values[subtrahend_column - 1]),
+                        ", Spread: %5s" 
+                        % ( abs( float(values[minuend_column - 1])
+                            - float(values[subtrahend_column - 1]))))
     
-        # Increments the linecount for each line
-        linecount = linecount + 1
-    
-    # Finds the minimum spread
-    minimalspread = filetoreaddata[0][1]
-    for eachindex in range ( len(filetoreaddata) ):
-        if filetoreaddata[eachindex][1] < minimalspread:
-            minimalspread = filetoreaddata[eachindex][1]
+    # These commands find the minimum spread.
+    minimal_spread = file_to_read_data[0][1]
+    for each_index in range(len(file_to_read_data)):
+        if file_to_read_data[each_index][1] < minimal_spread:
+            minimal_spread = file_to_read_data[each_index][1]
     
     # Finds the index or indexs with the minimum  spread
-    minimalspreadindex = []
-    for eachindex in range ( len(filetoreaddata) ):
-        if filetoreaddata[eachindex][1] == minimalspread:
-            minimalspreadindex.append( filetoreaddata[eachindex][0] )
+    minimal_spreadindex = []
+    for each_index in range(len(file_to_read_data)):
+        if file_to_read_data[each_index][1] == minimal_spread:
+            minimal_spreadindex.append(file_to_read_data[each_index][0])
     
     # Prints the index or indexs with the minimum  spread and the minimum  spread
-    print "\nFile: ", filenametoread, "; Index (Indices) with Minimum Spread: ", minimalspreadindex, "; Minimum Spread: ", minimalspread, "\n"
+    print("\nFile: ", filename_to_read, 
+            "; Index (Indices) with Minimum Spread: ", 
+            minimal_spreadindex, "; Minimum Spread: ", 
+            minimal_spread, "\n")
 
 # Calls the function with the parameters for the temperature file
-MinimumSpreadFinder(filenametoread = "w_data.dat", firstlinetoread = 7, firstlinenottoread = 37, indexcolumn = 1, minuendcolumn = 2, subtrahendcolumn = 3)
+minimum_spread_finder(filename_to_read = "w_data.dat", 
+        index_column = 1, minuend_column = 2, 
+        subtrahend_column = 3)
 # Calls the function with the parameters for the soccer file
-MinimumSpreadFinder(filenametoread = "soccer.dat", firstlinetoread = 4, firstlinenottoread = 25, indexcolumn = 2, minuendcolumn = 7, subtrahendcolumn = 8)
+minimum_spread_finder(filename_to_read = "soccer.dat", 
+        index_column = 2, minuend_column = 7, 
+        subtrahend_column = 8)
 
-print "Programming Challenge #1 was similar to Programming Challenge #2, influencing the second program to be more general."
-print "The second program calls the same function twice with different parameters."
-print "The first call finds the spread for the weather data file."
-print "The second call finds the spread for the soccer data file."
-print "Likewise, the same function could be used to find the spread on a different data file."
-print "The parameters used are filename, header and footer line indicators, and column indicators for the values used to calculate the spread."
-print "The minimum spread is returned as a value along with a list of indices for which it occurs."
-print "Absolute value is used to ensure the spread is positive, special characters are stripped from each line, and lines with no relevant data are ignored.\n"
-
+print("Programming Challenge #1 was similar") 
+print("to Programming Challenge #2,") 
+print("influencing the second program to be more general.\n")
+print("The second program calls the same function twice")
+print("with different parameters.")
+print("The first call finds the spread for the weather data file.")
+print("The second call finds the spread for the soccer data file.")
+print("Likewise, the same function could be used to find the spread")
+print("on a different data file.")
+print("The parameters used are filename")
+print("and column indicators for the values used to calculate the spread.\n")
+print ("The minimum spread is returned as a value along with a list of")
+print("indices for which it occurs.")
+print("Absolute value is used to ensure the spread is positive,")
+print("special characters are stripped from each line,")
+print("and lines with no relevant data are ignored.\n")
